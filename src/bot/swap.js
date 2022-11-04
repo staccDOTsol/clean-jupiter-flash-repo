3de18c200e5eb9097ce6ec151b865a1c92dc2706
Jarett Dunn <jarettrsdunn+git@gmail.com>
2022-11-04 13:50:07 -0300

    Hh
    Hh

--- src/bot/swap.js
+++ src/bot/swap.js
@@ -5,7 +5,7 @@ const JSBI = require('jsbi')
 const { getSwapResultFromSolscanParser } = require("../services/solscan");
 let { flashRepayReserveLiquidityInstruction, flashBorrowReserveLiquidityInstruction,SOLEND_PRODUCTION_PROGRAM_ID } = require('@solendprotocol/solend-sdk')
 const {  createTransferCheckedInstruction } = require('@solana/spl-token');
-
+const BN = require('bn.js')
 
 var  { SolendMarket } = require("@solendprotocol/solend-sdk");
 if (process.env.tradingStrategy == 'arbitrage'){
