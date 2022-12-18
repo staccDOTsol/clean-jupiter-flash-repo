1544d16f393d48d2a9b0da0641abfc6c77a618f0
jd <jd@pop-os.localdomain>
2022-12-18 13:54:45 -0400

    spot

--- prismtest/express.js
+++ prismtest/express.js
@@ -276,14 +276,14 @@ return
 						//  oldData[tokenb.address + token.address] =    maybe2.oldData
 						if (true) {
 							
-							let routes2 = prism.getRoutes(routes[abc].amountOut / 1.01); // get routes based on from Token amount 10 USDC -> ? PRISM
+							let routes2 = prism.getRoutes(routes[abc].amountOut / 1.009); // get routes based on from Token amount 10 USDC -> ? PRISM
 							console.log(routes2.length);
 							if (true) {
 								var bca = Math.floor(Math.random() * 2);
 								try {
 									if (routes2[bca]) {
 										console.log(
-											routes2[bca].amountOut > routes[abc].amountIn * 1.032
+											routes2[bca].amountOut > routes[abc].amountIn * 1.0105
 										);
 										if (
 											routes2[bca].amountOut > routes[abc].amountIn * 1.032 &&
