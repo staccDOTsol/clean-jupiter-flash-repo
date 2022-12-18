4be29fcb9e625326892bd4209bbb7107bd3e9eb5
jd <jd@pop-os.localdomain>
2022-12-18 16:37:28 -0400

    spot

--- prismtest/express.js
+++ prismtest/express.js
@@ -231,7 +231,7 @@ async function dothehorriblething(i, tokenbc, innn, dec) {
 		let symbol = reserve.config.liquidityToken.symbol;
 		//mod = Math.random() * 0.05 + 0.001;
 		const token = {
-			address: reserve.config.liquidityToken.address,
+			address: reserve.config.liquidityToken.mint,
 			decimals: reserve.config.liquidityToken.decimals,
 			symbol: reserve.config.asset,
 		};
