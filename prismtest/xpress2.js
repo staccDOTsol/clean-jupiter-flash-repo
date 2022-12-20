090675d8b33b0bd23f480b451d70b95dda7c1b53
jd <jd@pop-os.localdomain>
2022-12-20 10:00:59 -0400

    risk.lol comments

--- prismtest/xpress2.js
+++ prismtest/xpress2.js
@@ -406,7 +406,7 @@ try {
 		inputAmount: (amountToTrade) / 10 ** atokens[1].decimals,
 		slippageBps: 100})
   } catch (err){
-    totrades[tokenb.address] = 1
+    //totrades[tokenb.address] = 1
   }
 		const routes = someroutes[0]
 		const routes2 = someroutes[1]
